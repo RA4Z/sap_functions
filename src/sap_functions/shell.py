ad259389df81fc207eac53bbbe55993deb6793ee
robertn <robertn@weg.net>
2025-10-21 08:17:06 -0300

    Documentation of every Shell function

--- src/sap_functions/shell.py
+++ src/sap_functions/shell.py
@@ -7,6 +7,10 @@ class Shell:
         self.session = session
 
     def select_layout(self, layout: str) -> None:
+        """
+        This function will select the desired Shell Layout when a SAP select Layout Pop up is open
+        :param layout: The desired Layout name
+        """
         try:
             self.shell_obj.selectColumn("VARIANT")
             self.shell_obj.contextMenu()
@@ -21,6 +25,10 @@ class Shell:
             raise Exception("Select layout failed.")
 
     def count_rows(self) -> int:
+        """
+        This function will count all the rows in the current Shell
+        :return: A integer with the total number of rows in the current Shell
+        """
         try:
             rows = self.shell_obj.RowCount
             if rows > 0:
@@ -40,12 +48,23 @@ class Shell:
             raise Exception("Count rows failed.")
 
     def get_cell_value(self, index: int, column_id: str) -> str:
+        """
+        Get the value of a specific Shell cell
+        :param index: Row number of the desired cell
+        :param column_id: Shell column "Field Name" found in the respective column Technical Information tab
+        :return: The value of the cell
+        """
         try:
             return self.shell_obj.getCellValue(index, column_id)
         except:
             raise Exception("Get cell value failed.")
 
     def get_shell_content(self) -> dict:
+        """
+        Store all the content from a SAP Shell, the data will be stored and returned in a dictionary with 'header' and
+        'content' items
+        :return: A dictionary with 'header' and 'content' items
+        """
         try:
             grid_column = self.shell_obj.ColumnOrder
             rows = self.count_rows()
@@ -59,18 +78,30 @@ class Shell:
             raise Exception("Get all Shell Content Failed.")
 
     def select_all_content(self) -> None:
+        """
+        Select all the table, using the SAP native function to select all items
+        """
         try:
             self.shell_obj.selectAll()
         except:
             raise Exception("Select All Content Failed.")
 
     def select_column(self, column_id: int) -> None:
+        """
+        Select a specific column
+        :param column_id: Shell column "Field Name" found in the respective column Technical Information tab
+        """
         try:
             self.shell_obj.selectColumn(column_id)
         except:
             raise Exception("Select Column Failed.")
 
     def click_cell(self, index: int, column_id: str) -> None:
+        """
+        This function will select and double-click in a SAP Shell cell
+        :param index: Row number of the desired cell
+        :param column_id: Shell column "Field Name" found in the respective column Technical Information tab
+        """
         try:
             self.shell_obj.SetCurrentCell(index, column_id)
             self.shell_obj.doubleClickCurrentCell()
@@ -78,6 +109,11 @@ class Shell:
             raise Exception("Click Cell Failed.")
 
     def press_button(self, field_name: str, skip_error: bool = False) -> None:
+        """
+        This function will press any button in the SAP Shell component
+        :param field_name: The button that you want to press, this text need to be inside the button or in the tooltip of the button
+        :param skip_error: Skip this function if occur any error
+        """
         try:
             found = False
             for i in range(100):
@@ -92,6 +128,12 @@ class Shell:
             raise Exception("Press button failed")
 
     def press_nested_button(self, *nested_fields: str, skip_error: bool = False) -> None:
+        """
+        This function needs to receive several strings that have the texts that appear written in the button destination
+        that you want to press, it must be written in the order that it appears to reach the final button
+        :param nested_fields: The nested path that you want to navigate to press the button
+        :param skip_error: Skip this function if occur any error
+        """
         try:
             found = False
             for i in range(100):
