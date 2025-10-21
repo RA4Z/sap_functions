c5f0f7e1e03b2c34fd2a8e2aa4c9935e493e00fe
GabrielvMarinho <147559808+GabrielvMarinho@users.noreply.github.com>
2025-10-21 16:51:53 -0300

    table and shell test modules continued

--- tests/test_transaction_shell.py
+++ tests/test_transaction_shell.py
@@ -17,14 +17,18 @@ def test_transaction():
    sap.select_transaction(os.getenv("transaction_1"))
 
 def test_insert_data_transation():
-   print(os.getenv("transaction_1_field_1_name"), os.getenv("transaction_1_field_1_value"))
    sap.write_text_field(os.getenv("transaction_1_field_1_name"), os.getenv("transaction_1_field_1_value"))
 
 def test_run_transaction():
    sap.run_actual_transaction()
 
-def test_shell():
+
+shell = None
+def test_get_shell():
+   global shell
    shell = sap.get_shell()
+
+def test_shell_layout():
    with pytest.raises(Exception):
       exec(os.getenv("transaction_1_before_shell_layout"))
       shell.select_layout(os.getenv("transaction_1_shell_not_existant_layout"))
@@ -32,3 +36,21 @@ def test_shell():
    exec(os.getenv("transaction_1_before_shell_layout"))
    shell.select_layout(os.getenv("transaction_1_shell_layout"))
 
+def test_shell_get_content():
+   content = shell.get_shell_content()
+   assert type(content.get("header")).__name__ == "list"
+   assert type(content.get("content")).__name__ == "list"
+
+def test_shell_count_rows():
+   rows = shell.count_rows()
+   assert type(rows).__name__ == "int"
+
+def test_shell_get_cell_value():
+   cell_value = shell.get_cell_value(0, os.getenv("transaction_1_shell_column_id"))
+   assert type(cell_value).__name__ == "str"
+
+def test_shell_select_actions():
+   shell.select_all_content()
+   shell.select_column(os.getenv("transaction_1_shell_column_id"))   
+   shell.click_cell(0, os.getenv("transaction_1_shell_column_id"))
+   
