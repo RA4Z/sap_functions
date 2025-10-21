15feef510c664f43563603cb3f9695d31ebdf68c
robertn <robertn@weg.net>
2025-10-21 14:31:40 -0300

    Creating a function that return all labels in the SAP Screen

--- src/sap_functions/label.py
+++ src/sap_functions/label.py
@@ -6,6 +6,41 @@ class Label:
         self.session = session
         self.wnd = window
 
+    def get_all_screen_labels(self) -> list:
+        """
+        This function will return each label row in the SAP Screen
+        :return: A list with lists
+        """
+        finished_collecting = False
+        content = []
+        added_rows = []
+
+        while True:
+            if finished_collecting:
+                break
+            for i in range(2, 100):
+                active_row = []
+                for c in range(0, 35):
+                    try:
+                        cell = self.session.findById(f"wnd[{self.wnd}]/usr/lbl[{c},{i}]").text
+                        active_row.append(cell)
+                    except:
+                        pass
+
+                if not all(value is None for value in active_row):
+                    row_with_id = list(active_row)
+                    row_with_id.append(i)
+
+                    if row_with_id in added_rows:
+                        finished_collecting = True
+                        break
+
+                    content.append(active_row)
+                    added_rows.append(row_with_id)
+
+            self.session.findById(f"wnd[{self.wnd}]").sendVKey(82)
+        return content
+
     def get_label_content(self) -> dict:
         """
         Store all the content from a SAP Label, the data will be stored and returned in a dictionary with
