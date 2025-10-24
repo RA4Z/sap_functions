9e74f3aec91ca53633d675acf9838074969fe968
robertn <robertn@weg.net>
2025-10-24 08:57:06 -0300

    Creating tests for node components

---
+++ tests/test_transaction_node.py
@@ -0,0 +1,28 @@
+import sys
+from pathlib import Path
+sys.path.append(str(Path(__file__).resolve().parents[1]))
+
+from src.sap_functions import SAP
+import pytest
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+sap = SAP()
+
+
+def test_transaction():
+   with pytest.raises(Exception):
+      sap.select_transaction(os.getenv("not_existant_transaction"))
+   sap.select_transaction(os.getenv("transaction_4"))
+
+def test_select_project():
+   sap.press_button(os.getenv("transaction_4_open_pop_up"))
+   exec(os.getenv("transaction_4_pep_element"))
+   sap.run_actual_transaction()
+
+def test_expand_and_select_nodes():
+   node = sap.get_node()
+   node.expand_selected_node()
+   node.select_node(os.getenv("transaction_4_selected_node"))
